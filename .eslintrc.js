a03c56639ec21b9c8cb1a406a954d1ccbd16fd8c
George Pricop <96602369+Gzeu@users.noreply.github.com>
2025-09-19 04:16:45 +0300

    Add ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    es2022: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': 'off',
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'no-trailing-spaces': 'error',
+    'eol-last': 'error'
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'build/',
+    'coverage/',
+    'dist/'
+  ]
+};
